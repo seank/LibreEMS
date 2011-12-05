205f656cf3666592f6b3a215a3071fa92de77dec
Sean Keys <info@powerefi.com>
2011-12-05 14:26:47 -0700

    Increase defined size of xgate code block.

--- src/init.c
+++ src/init.c
@@ -24,7 +24,7 @@
  */
 
 
-/**	@file init.c
+/** @file
  *
  * @brief Initialise the devices state
  *
@@ -51,6 +51,7 @@
 #include "inc/init.h"
 #include "inc/decoderInterface.h"
 #include "inc/xgateVectors.h"
+#include "inc/xgateGlobals.h"
 #include <string.h>
 
 
@@ -558,42 +559,53 @@ void initFlash(){
  */
 void initXgate(){
 	/* route interrupt to xgate, vector address = channel_id * 2 */
-	ROUTE_INTERRUPT(0x39, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on software0 interrupt */
-	ROUTE_INTERRUPT(0x3A, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT3 */
-	ROUTE_INTERRUPT(0x3B, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT2 */
-	ROUTE_INTERRUPT(0x3C, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT1 */
-	ROUTE_INTERRUPT(0x3D, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT0 */
-
-	/* XGATE sees flash starting at paged address 0xE0, 0x8800 to + 30Kb*/
+	ROUTE_INTERRUPT(0x39, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE) /*enable xgate int on software0 interrupt */
+	//ROUTE_INTERRUPT(0x3A, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT3 */
+	//ROUTE_INTERRUPT(0x3B, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE ) /*enable xgate int on PIT2 */
+	ROUTE_INTERRUPT(0x3C, XGATE_INTERRUPT, PRIORITY_LEVEL_ONE) /*enable xgate int on PIT1 */
+	ROUTE_INTERRUPT(0x3D, XGATE_INTERRUPT, PRIORITY_LEVEL_TWO) /*enable xgate int on PIT0 */
+	/* XGATE sees flash starting at paged address 0xE0+0x8800 to + 30Kb*/
 	unsigned char savedRPAGE = RPAGE;
 	unsigned char savedPPAGE = PPAGE;
 	// XGATE threads execute from RAM
 	RPAGE = RPAGE_TUNE_TWO;
 	PPAGE = 0xE1;
 	// we can't use the symbols for the memcpy part because the symbols need to contain xgate relevant values
-	memcpy(START_OF_RAM_WINDOW, START_OF_FLASH_WINDOW, XGATE_RAM_ALLOCATION_SIZE);
+	memcpy(START_OF_RAM_WINDOW, START_OF_FLASH_WINDOW,
+			XGATE_RAM_ALLOCATION_SIZE);
 	//TODO set RAM protection
 	RPAGE = savedRPAGE;
 	PPAGE = savedPPAGE;
 	// Set the XGVBR register to its start address in flash (page 0xE0 after 2K register space)
-	XGVBR = (unsigned short )0x0800; // EO region is divided to ensure vectors end up here visible to xgate
-
+	XGVBR = (unsigned short) 0x0800; // EO region is divided to ensure vectors end up here visible to xgate
 	// Enable XGate and XGate interrupts
-	XGMCTL= (unsigned short)0x8181;
-
+	XGMCTL = (unsigned short) 0x8181;
 	/* Enable PIT TODO move back to proper section once unit tested */
-	PITMTLD0 = 0x1F; /* 32 prescaler gives 0.8uS resolution and max period of 52.4288ms measured */
-	PITMTLD1 = 0x1F; /* ditto */
+	//	PITMTLD0 = 0x1F; /* 32 prescaler gives 0.8uS resolution and max period of 52.4288ms measured */
+	/* Measurements from logic analizer PITMTLD0
+	 * 0x01 = 3.28ms@16-bits
+	 */
+	/* A prescalar of 4 yeilds .1ms resolution and max times of 6.5535ms@16-bit, 429,483.622ms@32-bit */PITMTLD0 =
+	0x1F; /* 32 prescaler (1 / ((40 MHz) / PRESCALAR)) */
+	PITMTLD1 = 0x1F; /* 32 prescaler  (1 / ((40 MHz) / PRESCALAR)) */
+	//PITMTLD0 = 0x03; /* 4 prescaler (1 / ((40 MHz) / PRESCALAR)) */
+	//PITMTLD1 = 0x03; /* 4 prescaler  (1 / ((40 MHz) / PRESCALAR)) */
 	PITMUX = 0xC0; /* set chan 0-1 to use PITMTLD0 base and chan 2-3 to use PITMTLD1 */
-	/* http://www.google.com/search?hl=en&safe=off&q=1+%2F+%2840MHz+%2F+32+%29 Exactly the same as for ECT */
-	PITLD0 = 0xFFFF; // set timers running //TEST ONLY
-	PITLD1 = 0x7FFF; // set timers running //TEST ONLY
-	PITLD2 = 0xFFFF; // set timers running //TEST ONLY
-
+	PITLD0 = 0x0000; // set timers running //TEST ONLY
+	PITLD1 = 0xFFFF; // set timers running //TEST ONLY
+	//PITLD2 = 0xFFFF; /* This is our metronome so this number is static */
+	//PITLD3 = 0x01; // set timers running //TEST ONLY
 	PITCFLMT = 0x80; // enable module
-	PITCE = 0x05; // enable channels 0, 1 and 2
-	PITINTE = 0x05; // enable interrupt on 0, 1 and 2
+	PITCE = 0x02; // countdown enable on our metronome
+	PITINTE = 0x03; // enable interrupt on 0, 1
 	PITFLT = ONES; // clear flags
+	/* Initialize our global xgate pointers */
+	XGOutputEvents = (XGOutputEvent*) (parametersBase
+			- RPAGE_TUNE_TWO_WINDOW_DIFFERENCE);
+	xgsInStamp = (unsigned short*) (xGSInputEdgeStamp
+			- RPAGE_TUNE_TWO_WINDOW_DIFFERENCE);
+	xgsEventsToSch = (unsigned short*) (xgsNumOfEventsToSchedule
+			- RPAGE_TUNE_TWO_WINDOW_DIFFERENCE);
 }
 
 /* Set up the timer module and its various interrupts */
