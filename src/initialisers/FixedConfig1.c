1b15287b1b77deabf2a9e30f24a439c50bc3d642
Sean Keys <skeys@ipdatasys.com>
2012-10-16 08:39:29 -0700

    Revised config for my R1 engine.  I belive this should be correct.  AnglesOfTDC for fuel events have been offset away from actual TDC, this should work ok and give me the ability to tune where I want my injections to occur.

--- src/initialisers/FixedConfig1.c
+++ src/initialisers/FixedConfig1.c
@@ -172,14 +172,12 @@ const volatile fixedConfig1 fixedConfigs1 FIXEDCONF1 = {
 		numberOfInjectionsPerEngineCycle:        1  // Sequential, baby, yeah!
 
 #elif CONFIG == SEANKR1_ID // Firing order 1-2-3-4
-		/*TODO finish, I'll have to reread the scheduler code, I'm curious as to how this handles
-		 a situation where you have more event channels than anglesOfTDC. Maybe create xgateOutputEventChannel:? */
-		anglesOfTDC: {ANGLE(0), ANGLE(45), ANGLE(90), ANGLE(135), ANGLE(180), ANGLE(225), ANGLE(270), ANGLE(315), ANGLE(360), ANGLE(405), ANGLE(450), ANGLE(495), ANGLE(540), ANGLE(585), ANGLE(540), ANGLE(630)},
-		outputEventPinNumbers:       {0,1,2,3,4,5}, // COP/CNP ignition only
-		schedulingConfigurationBits: {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, // All ignition
+		anglesOfTDC: {ANGLE(0), ANGLE(180), ANGLE(360), ANGLE(540), ANGLE(45), ANGLE(90), ANGLE(145), ANGLE(270), ANGLE(315), ANGLE(450), ANGLE(495), ANGLE(630)},
+		outputEventPinNumbers:       {0,1,2,3,4,5,4,5,4,5,4,5}, // COP/CNP ignition only
+		schedulingConfigurationBits: {0,0,0,0,1,1,1,1,1,1,1,1}, // First 4 ign, 8 fuel
 		decoderEngineOffset:           ANGLE(0.00), //
-		numberOfConfiguredOutputEvents:         16, //
-		numberOfInjectionsPerEngineCycle:        1  // Sequential ignition, Sequential fuel(via XGATE) :-p
+		numberOfConfiguredOutputEvents:          12, //
+		numberOfInjectionsPerEngineCycle:        4  // keep pulsewidths small, so that we can at leat reference their starting point against xgate ouput
 
 #else // Nothing scheduled by default, no sensible default for all possible vehicle setups.
 		anglesOfTDC:                            {}, // Depends on cylinder count and other variables
