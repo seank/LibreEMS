43531fe5ca1bfe953aa278c18d050e35809efbfa
Sean Keys <skeys@athenawave.com>
2014-02-06 22:31:08 -0700

    Remove EXTERN macro and it's use from staticBenchTest files.

--- src/main/StaticBenchTest.c
+++ src/main/StaticBenchTest.c
@@ -1,3 +1,25 @@
+/* LibreEMS - the community driven open source engine management system
+ *
+ * Copyright 2014 Sean Keys
+ *
+ * This file is part of the LibreEMS project.
+ *
+ * FreeEMS software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeEMS software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with any LibreEMS software.  If not, see http://www.gnu.org/licenses/
+ *
+ * Thank you for choosing LibreEMS to run your engine!
+ */
+
 /* FreeEMS - the open source engine management system
  *
  * Copyright 2011 Fred Cooke
@@ -36,6 +58,10 @@
  */
 
 
-#define STATIC_BENCH_TEST_C
 #include "decoders/inc/BenchTest.h"
-// No code! Just the flag and included header which looks for it! The End!
+
+unsigned short testTicksPerEvent;
+unsigned short testNumberOfCycles;
+unsigned char testEventsPerCycle;
+unsigned char testNumberOfMissing;
+unsigned char testMode;
