971c75e39548008e904051d5c024d42ebbbd71d9
Sean Keys <skeys@athenawave.com>
2014-03-09 13:48:19 -0700

    Format corrections.

--- src/main/inc/packetTypes.h
+++ src/main/inc/packetTypes.h
@@ -48,36 +48,32 @@
 /*&&&&&&&&&&&&&&&&&&&&&&&&  Protocol Payload Type IDs  &&&&&&&&&&&&&&&&&&&&&&&*/
 
 /* Firmware Independent functions */
-#define REQUEST_INTERFACE_VERSION     0x0000
-#define REQUEST_FIRMWARE_VERSION      0x0002
+#define REQUEST_INTERFACE_VERSION      0x0000
+#define REQUEST_FIRMWARE_VERSION       0x0002
 #define REQUEST_MAX_PACKET_SIZE        0x0004
 #define REQUEST_ECHO_PACKET_RETURN     0x0006
 #define REQUEST_SOFT_SYSTEM_RESET      0x0008 /* System comes up fresh, so no response */
-//efine REPLYTO_SOFT_SYSTEM_RESET      0x0009 /* This is reserved */
+//efine REPLYTO_SOFT_SYSTEM_RESET       0x0009 /* This is reserved */
 #define REQUEST_HARD_SYSTEM_RESET      0x000A /* System comes up fresh, so no response */
-//efine REPLYTO_HARD_SYSTEM_RESET      0x000B /* This is reserved */
+//efine REPLYTO_HARD_SYSTEM_RESET       0x000B /* This is reserved */
 #define REQUEST_RE_INIT_OF_SYSTEM       0x000C /* EXPERIMENTAL, currently responds like normal */
 //efine REPLYTO_RE_INIT_OF_SYSTEM       0x000D /* This is reserved */
-
-
 //efine REQUEST_ASYNC_DEBUG_INFO       0x000E /* This is reserved */
 #define ASYNC_DEBUG_INFO_PACKET        0x000F /* NOTE : Unrequested debug information packet */
 //efine REQUEST_ASYNC_ERROR_CODE       0x0010 /* This is reserved */
 #define ASYNC_ERROR_CODE_PACKET        0x0011 /* NOTE : Unrequested error event code packet */
 
 
-/*&&&&&&&&&&&&&&&&&&&&&&&&  Firmware Payload Type IDs  &&&&&&&&&&&&&&&&&&&&&&&*/
-
 /* Data block manipulation */
 #define UPDATE_BLOCK_IN_RAM            0x0100
 #define UPDATE_BLOCK_IN_FLASH          0x0102
 #define RETRIEVE_BLOCK_FROM_RAM        0x0104
 #define RETRIEVE_BLOCK_FROM_FLASH      0x0106
-#define BURN_BLOCK_FROM_RAM_TO_FLASH     0x0108
+#define BURN_BLOCK_FROM_RAM_TO_FLASH   0x0108
 
 /* Datalog request packets */
-#define REQUEST_DATALOG_PACKET        0x0190
-#define RESPONSE_BASIC_DATALOG        0x0191 /* Defined because it can be used both synchronously and asynchronously */
+#define REQUEST_DATALOG_PACKET         0x0190
+#define RESPONSE_BASIC_DATALOG         0x0191 /* Defined because it can be used both synchronously and asynchronously */
 #define REQUEST_BYTE_LA_DATALOG        0x0196 // logic analyser
 #define RESPONSE_BYTE_LA_DATALOG       0x0197 // logic analyser
 
